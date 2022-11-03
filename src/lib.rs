e25853efe9ee42bc13d1ab9af81887a090cde0f8
Jonathan Kelley <jkelleyrtp@gmail.com>
2022-11-03 16:59:23 -0700

    feat: add lifetimes

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,57 @@
 use std::future::Future;
 
-trait AnyFn {}
+/// A trait that lets us do something with the fn itself
+trait AnyFn {
+    fn call(self, scope: &Scope) -> SuspenseMode;
+}
 
 struct Scope;
+
+impl Scope {
+    fn alloc<T>(&self, val: T) -> &T {
+        todo!()
+    }
+}
 struct Element;
 
-trait ReturnType<M = ()> {}
+trait ReturnType<'a, M = ()> {
+    fn alloc(self, scope: &'a Scope) -> SuspenseMode<'a>;
+}
 
 struct AsyncMarker;
 
-impl ReturnType for Element {}
+impl<'a> ReturnType<'a> for Element {
+    fn alloc(self, scope: &'a Scope) -> SuspenseMode<'a> {
+        SuspenseMode::Sync(scope.alloc(self))
+    }
+}
 
 enum SuspenseMode<'a> {
     Sync(&'a Element),
     Async(Box<dyn Future<Output = Element> + 'a>),
 }
-impl<F> ReturnType for F where F: Future<Output = Element> {}
-impl<F: ReturnType> AnyFn for fn(Scope) -> F {}
+impl<'a, F> ReturnType<'a> for F
+where
+    F: Future<Output = Element> + 'a,
+{
+    fn alloc(self, scope: &'a Scope) -> SuspenseMode<'a> {
+        SuspenseMode::Async(Box::new(self))
+    }
+}
+
+impl<'a, F: ReturnType<'a>> AnyFn for fn(&'a Scope) -> F {
+    fn call(self, scope: &Scope) -> SuspenseMode {
+        todo!()
+    }
+}
 
 fn it_goes(f: impl AnyFn) {}
 
-fn example(cx: Scope) -> Element {
+fn example(cx: &Scope) -> Element {
     Element
 }
 
-async fn example2(cx: Scope) -> Element {
+async fn example2(cx: &Scope) -> Element {
     Element
 }
 
